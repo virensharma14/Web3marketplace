362595e2ae6a22a41c0945ac62b376b9a0103af6
Viren Sharma <96727156+virensharma14@users.noreply.github.com>
2023-01-22 20:57:49 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+
+const App = () => {
+ return (
+   <div className="min-h-screen">
+     <div className="gradient-bg-hero">
+       <Header />
+     </div>
+   </div>
+ )
+}
+
+export default App
